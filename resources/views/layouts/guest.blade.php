e1cf2e13f4baf797407d2a67e1588122db814cb7
rzkynovan <rizkynovan21@gmail.com>
2024-09-14 00:43:02 +0700

    init project

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+        </style>
+
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-[sans-serif]">
+        {{$slot}}
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-guest-layout>
+    <h1>Tess</h1>
+</x-guest-layout>
